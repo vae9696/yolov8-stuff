36c038f04a0a482ff18b1e5d3bebd66cd9e4b9bb
vae9696 <61275054+vae9696@users.noreply.github.com>
2024-06-04 21:09:46 +0800

    t1 (最醜的版本,don

--- flaskapi.py
+++ flaskapi.py
@@ -35,54 +35,42 @@ for-data:{
 @app.route("/CallPredic",methods = ["POST"]) #API接口
 def CallPredict():
     if request.method == 'POST':
-        if request.values.get("numbers"): #判断是否有numbers字段
-            print("User send {x} images.".format(x = request.values.get("numbers")))
-            numbers = int(request.values.get("numbers"))
-            images = []
-            for i in range(1,numbers+1): 
-                tempstr = "image" + str(i)
-                if request.files.get(tempstr): #判断是否有imagex字段
-                    result = request.files.get(tempstr)
-                    if(allowed_file(result.filename)): #检查文件扩展名是否合法
-                        result.save(os.path.join(app.config['UPLOAD_FOLDER'], result.filename))  #暂存图片
-                        print("Save image: {x}".format(x = result.filename))
-                        images.append(r'CodeSavePlace/data/pred-use/tempsave/' + result.filename) #暂存图片路由
-                    else:
-                        print("{x} is not corrrect in ALLOWED_EXTENSIONS".format(x = result.filename))
-                        return jsonify(Error_msg = "{x} is not corrrect in ALLOWED_EXTENSIONS".format(x = result.filename)) #文件扩展名不合法
-                else:
-                    temp_delete = os.listdir(r'CodeSavePlace/data/pred-use/tempsave') #暂存刪除图片路由
-                    for res_file_name in temp_delete:
-                        os.remove(r'CodeSavePlace/data/pred-use/tempsave/'+res_file_name) #刪除暂存图片
-                    print("Cant get key {x} data".format(x = tempstr))
-                    return jsonify(Error_msg = "Cant get key {x} data".format(x = tempstr)) #没有这个imagex字段, 取值失败
-            try:
-               total_result = predict(images,True)
-               send_result = {} #返回结果容器
-               for i in range(0,len(total_result)): #构建结果容器(dict)字段
-                    send_result.setdefault("image"+str(i),{})
-                    res = json.loads(total_result[i])
-                    j = 0
-                    for r in res:
-                        print("{i} -- {name} : {confidence}".format(i = i, name = r["name"], confidence = r["confidence"]))
-                        send_result["image"+str(i)].setdefault(j,{r["name"] : r["confidence"]})
-                        j = j + 1
-               print("Find result is:")
-               print(send_result)
-               send_result = json.dumps(send_result) #转化为json数据(dict -> json)
-               for i in range(len(images)): #刪除暂存图片
-                   os.remove(images[i])
-               return send_result  #返回结果
-            except Exception as e: #检测Error
-                print("Predict function error!!!")
-                print(e)
-                return jsonify(Error_msg = "Predict function error!!!")
-
-        else:
-            print("Don't know user send how much images") #没有numbers字段
-            return jsonify(Error_msg = "You need to send numbers of how many images.")
+        #print(request.data)
+        img_bdata = request.data
+        file_path  = r"CodeSavePlace/data/pred-use/tempsave/comtest1.jpg"
+        with open(file_path, "wb") as f: 
+            f.write(img_bdata)
+            print("image save success!") 
+        print(type(request.data))
+        #result = str(request.data,encoding="utf-8")
+        #print("result is:{r}".format(r = result))
+        try:
+            total_result = predict(file_path,True)
+            print(total_result)
+            send_result = {} #返回结果容器
+            for i in range(0,len(total_result)): #构建结果容器(dict)字段
+                 send_result.setdefault("image"+str(i),{})
+                 res = json.loads(total_result[i])
+                 j = 0
+                 for r in res:
+                     print("{i} -- {name} : {confidence}".format(i = i, name = r["name"], confidence = r["confidence"]))
+                     send_result["image"+str(i)].setdefault(j,{r["name"] : r["confidence"]})
+                     j = j + 1
+            print("Find result is:")
+            print(send_result)
+            #print(send_result['image0'].keys())
+            #print(send_result['image0'][0].keys())
+            temp_list = list(send_result['image0'][0].keys())
+            #print(temp_list[0])
+            #print(send_result['image0'][0][temp_list[0]])
+            send_food_name = temp_list[0]
+            print("send_food_name is :{n}".format(send_food_name))
+            return send_food_name
+        except Exception as e:
+            print(e)
+            return "wtffff"
     else:
-        return jsonify(msg = "method wrong!!, it should be POST!!!!") #methods不为post
+        return "method wrong!!, it should be POST!!!!" #methods不为post
 
 
 
